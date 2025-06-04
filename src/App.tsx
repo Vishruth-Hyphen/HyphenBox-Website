0ce0a8cbec05b35b1d48b256519d9e459815f916
kiluazen <kushalsokke@gmail.com>
2025-06-04 13:28:22 +0530

    more analytics stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useEffect } from "react";
 import Index from "./pages/Index";
@@ -8,6 +8,37 @@ import PrivacyPolicy from "./pages/PrivacyPolicy";
 
 // Create a client
 const queryClient = new QueryClient();
+
+// Declare gtag function for TypeScript
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+    dataLayer: any[];
+  }
+}
+
+function AppRoutes() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // Track page views when location changes
+    if (window.gtag) {
+      window.gtag('config', 'G-TZBKR8D281', {
+        page_path: location.pathname + location.search,
+        page_title: document.title
+      });
+    }
+  }, [location]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/og-image" element={<OGImage />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
 
 function App() {
   useEffect(() => {
@@ -22,12 +53,25 @@ function App() {
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-      gtag('config', 'G-TZBKR8D281');
+      gtag('config', 'G-TZBKR8D281', {
+        send_page_view: false // We'll manually send page views
+      });
     `;
     document.head.appendChild(script2);
 
+    // Send initial page view after a short delay to ensure gtag is ready
+    const timer = setTimeout(() => {
+      if (window.gtag) {
+        window.gtag('config', 'G-TZBKR8D281', {
+          page_path: window.location.pathname + window.location.search,
+          page_title: document.title
+        });
+      }
+    }, 100);
+
     // Cleanup function
     return () => {
+      clearTimeout(timer);
       if (document.head.contains(script1)) {
         document.head.removeChild(script1);
       }
@@ -40,12 +84,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/og-image" element={<OGImage />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <AppRoutes />
       </BrowserRouter>
     </QueryClientProvider>
   );
